f199270b473e37a627e0db3d786823c745008e09
langoustinee <xmun777@naver.com>
2024-08-14 02:18:56 +0900

    🔧 입력 메시지 관리 열거형 인터페이스 개발
    - InputMessage Enum 타입 추가
    - InputView 클래스의 하드코딩 출력 메시지 변수 주입

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,25 +2,27 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static racingcar.common.InputMessage.*;
+
 public class InputView {
 
     public static String inputCarName() {
         try {
-            System.out.println("경주할 자동차 이름을 입력하세요. 자동차 이름은 쉼표를 기준으로 구분하여 입력해주세요.");
+            OutputView.printMessage(CAR_NAMES_MESSAGE.getValue());
             return Console.readLine();
         } catch (IllegalArgumentException exception) {
-            System.out.println(exception.getMessage());
+            OutputView.printMessage(exception.getMessage());
             return inputCarName();
         }
     }
 
     public static String inputTryCount() {
         try {
-            System.out.println("경주를 시도할 횟수를 입력해주세요.");
+            OutputView.printMessage(TRY_COUNT_MESSAGE.getValue());
             return Console.readLine();
         } catch (IllegalArgumentException exception) {
-            System.out.println(exception.getMessage());
-            return inputTryCount();
+            OutputView.printMessage(exception.getMessage());
+            return inputCarName();
         }
     }
 

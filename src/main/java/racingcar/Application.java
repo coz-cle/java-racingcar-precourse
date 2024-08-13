123f3d2f308b0483c2634b5d1451d1612fe8c88f
김성민 <sm.kim2@okestro.com>
2024-08-13 18:16:04 +0900

    :sparkles: 자동차 이름 validation 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,36 @@
 package racingcar;
 
+import java.util.*;
+
 public class Application {
+    private static final String EXCEPTION_MESSAGE_PREFIX = "[ERROR] ";
+
     public static void main(String[] args) {
         // TODO 구현 진행
+    }
+
+    private static void validateCarName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throwException("자동차의 이름은 공백이 아니여야 한다.");
+        }
+
+        if (name.length() > 5) {
+            throwException("자동차의 이름은 5자 이하여야 한다.");
+        }
+    }
+
+    private static void validateCarNames(List<String> names) {
+        if (names == null || names.isEmpty()) {
+            throwException("자동차의 이름은 하나라도 입력되어야 한다.");
+        }
+
+        Set<String> nameSet = new HashSet<>(names);
+        if(nameSet.size() != names.size()) {
+            throwException("자동차의 이름은 중복되지 않아야 한다.");
+        }
+    }
+
+    private static void throwException(String message) {
+        throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX + message);
     }
 }

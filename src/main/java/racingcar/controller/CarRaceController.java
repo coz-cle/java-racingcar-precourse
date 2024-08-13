d3ea8f614e2878d62761c45192c66864425fea4b
langoustinee <xmun777@naver.com>
2024-08-14 02:28:19 +0900

    ✨ 자동차 경주 컨트롤러 기능 확장
    - 입력값 요구할 수 있도록 inputView와 협력
    - 자동차 목록 및 시도횟수 유효성 검사 진행
    - CarService와 협력하여 Cars 일급 객체 제공

--- src/main/java/racingcar/controller/CarRaceController.java
+++ src/main/java/racingcar/controller/CarRaceController.java
@@ -1,20 +1,50 @@
 package racingcar.controller;
 
+import racingcar.common.CarNameValidator;
+import racingcar.common.TryCountValidator;
+import racingcar.domain.Cars;
+import racingcar.service.CarsService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class CarRaceController {
 
     private final InputView inputView;
     private final OutputView outputView;
 
+    private final CarsService carsService;
+
     public CarRaceController() {
         inputView = new InputView();
         outputView = new OutputView();
+        carsService = new CarsService();
     }
 
     public void startRace() {
-        // TODO: 경주 시작 비즈니스 구현 필요
+        String carNames = getCatNamesInput();
+        CarNameValidator carValidation = new CarNameValidator(carNames);
+        Cars cars = createCars(carValidation.CAR_NAMES);
+        // TODO: 자동차 목록 유효성 검사 및 객체화 완료
+
+        String tryCount = getTryCountInput();
+        TryCountValidator tryCountValidator = new TryCountValidator(tryCount);
+        // TODO: 경주 시도횟수 유효성 검사 완료
+
+    }
+
+    private Cars createCars(List<String> inputCarNames) {
+        return carsService.createCars(inputCarNames);
+    }
+
+
+    private String getCatNamesInput() {
+        return InputView.inputCarName();
+    }
+
+    private String getTryCountInput() {
+        return InputView.inputTryCount();
     }
 
 }

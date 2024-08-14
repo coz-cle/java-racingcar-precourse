f969123d532fb72990c0317fd466b2cd48a802a5
문태준 <tj.mun@okestro.com>
2024-08-14 17:51:20 +0900

    ✨ 유효성 검사 클래스 테스트 클래스 개발
    - 자동차 목록 및 시도횟수 검증 객체 로직 검증을 위한 테스트 케이스 개발

---
+++ src/test/java/racingcar/common/CarNameValidatorTest.java
@@ -0,0 +1,57 @@
+package racingcar.common;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.common.Constant.*;
+
+class CarNameValidatorTest {
+
+    @Test
+    @DisplayName("정상적인 자동차 목록을 입력받으면 CarNameValidator 객체를 생성할 수 있다.")
+    void validateSuccess() {
+        // given
+        String inputCarNames = "tjmun,smkim,sekim,kyu";
+        // when
+        CarNameValidator result = new CarNameValidator(inputCarNames);
+        // then
+        assertThat(result).isInstanceOf(CarNameValidator.class);
+    }
+
+    @Test
+    @DisplayName("자동차 목록의 자동차 이름이 문자나 숫자가 아닐 경우 예외가 발생한다.")
+    void validateCarNameWrongString() {
+        // given
+        String inputCarNames = "!!!,smkim,sekim,kyu";
+        // when & then
+        assertThatThrownBy(() -> new CarNameValidator(inputCarNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_NAME_WRONG_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("자동차 목록의 자동차 이름이 5자를 초과할 경우 예외가 발생한다.")
+    void validateCarNameLimitOver() {
+        // given
+        String inputCarNames = "tjmoon,smkim,sekim,kyu";
+        // when & then
+        assertThatThrownBy(() -> new CarNameValidator(inputCarNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_NAME_SIZE_OVER_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("자동차 목록에서 중복된 자동차 이름이 입력될 경우 예외가 발생한다.")
+    void validateCarNameDuplicate() {
+        // given
+        String inputCarNames = "tjmun,smkim,sekim,tjmun";
+        // when & then
+        assertThatThrownBy(() -> new CarNameValidator(inputCarNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CAR_NAME_DUPLICATE_MESSAGE);
+    }
+
+}
---
+++ src/test/java/racingcar/common/TryCountValidatorTest.java
@@ -0,0 +1,43 @@
+package racingcar.common;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class TryCountValidatorTest {
+
+    @Test
+    @DisplayName("입력받은 시도 횟수가 1 이상의 정수라면 TryCountValidator 객체를 생성할 수 있다.")
+    void validateSuccess() {
+        // given
+        String inputTryCount = "3";
+        // when & then
+        TryCountValidator result = new TryCountValidator(inputTryCount);
+        assertThat(result).isInstanceOf(TryCountValidator.class);
+    }
+
+    @Test
+    @DisplayName("입력받은 시도 횟수가 정수가 아닐 경우 예외가 발생한다.")
+    void validateTryCountIsNotIntNumber() {
+        // given
+        String inputTryCount = "0.2";
+        // when & then
+        assertThatThrownBy(() -> new TryCountValidator(inputTryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Constant.TRY_COUNT_OUT_OF_RANGE_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("입력받은 시도 횟수가 0일 경우 예외가 발생한다.")
+    void validateTryCountIsZero() {
+        // given
+        String inputTryCount = "0";
+        // when & then
+        assertThatThrownBy(() -> new TryCountValidator(inputTryCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(Constant.TRY_COUNT_OUT_OF_RANGE_MESSAGE);
+    }
+
+}

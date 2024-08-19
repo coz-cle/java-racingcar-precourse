4ec6d5939e1cfaaee241b11860c1ab24f68b984d
김성민 <sm.kim2@okestro.com>
2024-08-20 07:53:05 +0900

    :recycle: 입력에 대한 validation 메세지 상수화

--- src/main/java/racingcar/Input/CarNameProcessor.java
+++ src/main/java/racingcar/Input/CarNameProcessor.java
@@ -9,6 +9,12 @@ import java.util.Set;
 
 public class CarNameProcessor extends InputProcessor<List<String>> {
 	
+	private static final String CAR_NAME_REQUIRED_MESSAGE = "자동차의 이름은 하나라도 입력되어야 한다.";
+	private static final String CAR_NAME_NON_DUPLICATE_MESSAGE = "자동차의 이름은 중복되지 않아야 한다.";
+	private static final String CAR_NAME_NOT_EMPTY_MESSAGE = "자동차의 이름은 공백이 아니여야 한다.";
+	private static final String CAR_NAME_NOT_BLANK_MESSAGE = "자동차의 이름에는 공백이 없어야 한다.";
+	private static final String CAR_NAME_LIMITS_MESSAGE = "자동차의 이름은 5자 이하여야 한다.";
+	
 	public CarNameProcessor(InputScanner inputScanner) {
 		super(inputScanner);
 	}
@@ -26,27 +32,29 @@ public class CarNameProcessor extends InputProcessor<List<String>> {
 	
 	private void validateCarNames(List<String> names) {
 		if (names == null || names.isEmpty()) {
-			super.throwException("자동차의 이름은 하나라도 입력되어야 한다.");
+			super.throwException(CAR_NAME_REQUIRED_MESSAGE);
 		}
 		
 		Set<String> nameSet = new HashSet<>(names);
 		if (nameSet.size() != names.size()) {
-			super.throwException("자동차의 이름은 중복되지 않아야 한다.");
+			super.throwException(CAR_NAME_NON_DUPLICATE_MESSAGE);
 		}
 	}
 	
 	private void validateCarName(String name) {
 		
 		if (name == null || name.isEmpty()) {
-			super.throwException("자동차의 이름은 공백이 아니여야 한다.");
-		}
-		
-		if (name.length() > 5) {
-			super.throwException("자동차의 이름은 5자 이하여야 한다.");
+			super.throwException(CAR_NAME_NOT_EMPTY_MESSAGE);
 		}
 		
 		if (name.contains(" ")) {
-			super.throwException("자동차의 이름에는 공백이 없어야 한다.");
+			super.throwException(CAR_NAME_NOT_BLANK_MESSAGE);
 		}
+		
+		if (name.length() > 5) {
+			super.throwException(CAR_NAME_LIMITS_MESSAGE);
+		}
+		
+		
 	}
 }
--- src/main/java/racingcar/Input/TotalRoundProcessor.java
+++ src/main/java/racingcar/Input/TotalRoundProcessor.java
@@ -4,6 +4,10 @@ import racingcar.utils.InputScanner;
 
 public class TotalRoundProcessor extends InputProcessor<Integer> {
 	
+	private final static String TOTAL_ROUND_REQUIRED_MESSAGE = "시도할 횟수는 반드시 입력되어야 한다.";
+	private final static String TOTAL_ROUND_ONLY_NUMBER_MESSAGE = "시도할 횟수는 숫자만 입력되어야 한다.";
+	private final static String TOTAL_ROUND_MINIMUM_MESSAGE = "시도할 횟수는 1 이상이여야 한다.";
+	
 	public TotalRoundProcessor(InputScanner inputScanner) {
 		super(inputScanner);
 	}
@@ -16,16 +20,16 @@ public class TotalRoundProcessor extends InputProcessor<Integer> {
 	
 	private void validateTotalRound(String inputValue) {
 		if (inputValue == null || inputValue.trim().isEmpty()) {
-			throwException("시도할 횟수는 입력되어야 한다.");
+			throwException(TOTAL_ROUND_REQUIRED_MESSAGE);
 		}
 		
 		if (!isConvertibleToInt(inputValue)) {
-			throwException("시도할 횟수는 숫자만 입력되아야 한다.");
+			throwException(TOTAL_ROUND_ONLY_NUMBER_MESSAGE);
 		}
 		
 		final int totalRound = convertStringToInt(inputValue);
-		if (totalRound <= 0) {
-			throwException("시도할 횟수는 1 이상이여야 한다.");
+		if (totalRound < 1) {
+			throwException(TOTAL_ROUND_MINIMUM_MESSAGE);
 		}
 	}
 	

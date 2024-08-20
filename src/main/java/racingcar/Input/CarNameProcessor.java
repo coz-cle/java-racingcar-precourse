86b342a0f793ed4f409d6100089690354eabdc9f
김성민 <sm.kim2@okestro.com>
2024-08-20 09:43:34 +0900

    :recycle: 자동차 이름 입력 값 구분자 상수화

--- src/main/java/racingcar/Input/CarNameProcessor.java
+++ src/main/java/racingcar/Input/CarNameProcessor.java
@@ -9,11 +9,13 @@ import java.util.Set;
 
 public class CarNameProcessor extends InputProcessor<List<String>> {
 	
-	private static final String CAR_NAME_REQUIRED_MESSAGE = "자동차의 이름은 하나라도 입력되어야 한다.";
-	private static final String CAR_NAME_NON_DUPLICATE_MESSAGE = "자동차의 이름은 중복되지 않아야 한다.";
-	private static final String CAR_NAME_NOT_EMPTY_MESSAGE = "자동차의 이름은 공백이 아니여야 한다.";
-	private static final String CAR_NAME_NOT_BLANK_MESSAGE = "자동차의 이름에는 공백이 없어야 한다.";
-	private static final String CAR_NAME_LIMITS_MESSAGE = "자동차의 이름은 5자 이하여야 한다.";
+	private static final String NAME_SEPARATOR = ",";
+	private static final String REQUIRED_MESSAGE = "자동차의 이름은 하나라도 입력되어야 한다.";
+	private static final String NON_DUPLICATE_MESSAGE = "자동차의 이름은 중복되지 않아야 한다.";
+	private static final String NOT_EMPTY_MESSAGE = "자동차의 이름은 공백이 아니여야 한다.";
+	private static final String NOT_BLANK_MESSAGE = "자동차의 이름에는 공백이 없어야 한다.";
+	private static final String LIMITS_MESSAGE = "자동차의 이름은 5자 이하여야 한다.";
+	
 	
 	public CarNameProcessor(InputScanner inputScanner) {
 		super(inputScanner);
@@ -21,7 +23,7 @@ public class CarNameProcessor extends InputProcessor<List<String>> {
 	
 	@Override
 	List<String> validateInput(String input) {
-		List<String> names = Arrays.asList(input.split(","));
+		List<String> names = Arrays.asList(input.split(NAME_SEPARATOR));
 		
 		validateCarNames(names);
 		for (String name : names) {
@@ -32,27 +34,27 @@ public class CarNameProcessor extends InputProcessor<List<String>> {
 	
 	private void validateCarNames(List<String> names) {
 		if (names == null || names.isEmpty()) {
-			super.throwException(CAR_NAME_REQUIRED_MESSAGE);
+			super.throwException(REQUIRED_MESSAGE);
 		}
 		
 		Set<String> nameSet = new HashSet<>(names);
 		if (nameSet.size() != names.size()) {
-			super.throwException(CAR_NAME_NON_DUPLICATE_MESSAGE);
+			super.throwException(NON_DUPLICATE_MESSAGE);
 		}
 	}
 	
 	private void validateCarName(String name) {
 		
 		if (name == null || name.isEmpty()) {
-			super.throwException(CAR_NAME_NOT_EMPTY_MESSAGE);
+			super.throwException(NOT_EMPTY_MESSAGE);
 		}
 		
 		if (name.contains(" ")) {
-			super.throwException(CAR_NAME_NOT_BLANK_MESSAGE);
+			super.throwException(NOT_BLANK_MESSAGE);
 		}
 		
 		if (name.length() > 5) {
-			super.throwException(CAR_NAME_LIMITS_MESSAGE);
+			super.throwException(LIMITS_MESSAGE);
 		}
 		
 		

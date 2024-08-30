cd597fbe239b68886be427812333f3c7b88e60a9
김성민 <sm.kim2@okestro.com>
2024-08-30 17:50:47 +0900

    :recycle: 입력 값 검증 반복문 개선 - try-catch 문에 재귀 호출을 활용

--- src/main/java/racingcar/Input/InputProcessor.java
+++ src/main/java/racingcar/Input/InputProcessor.java
@@ -4,29 +4,25 @@ import racingcar.utils.InputScanner;
 
 public abstract class InputProcessor<T> {
 	private static final String EXCEPTION_MESSAGE_PREFIX = "[ERROR] ";
-	
+
 	private final InputScanner inputScanner;
-	
+
 	public InputProcessor(InputScanner inputScanner) {
 		this.inputScanner = inputScanner;
 	}
-	
+
 	public final T process() {
-		T result;
-		while (true) {
-			try {
-				result = validateInput(inputScanner.readLine());
-				break;
-			} catch (IllegalArgumentException e) {
-				System.out.println(e.getMessage());
-			}
+		try {
+			return validateInput(inputScanner.readLine());
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return process();
 		}
-		return result;
 	}
-	
+
 	protected void throwException(String message) {
 		throw new IllegalArgumentException(EXCEPTION_MESSAGE_PREFIX + message);
 	}
-	
+
 	abstract T validateInput(String input);
 }
